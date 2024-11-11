fe9af1a87a0ea325ad5aa6fbcec96de4a1d6f013
ansu <annastiina.koivu@gmail.com>
2024-11-11 13:30:27 +0200

    lisää Store- ja Settings-komponentit

---
+++ src/pages/Store.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+
+function Store(props) {
+
+    return (
+        <div className="container">
+            <Header balance={props.stats.balance}>store</Header>
+            <div className="scrollbox items">
+                TODO items
+            </div>
+        </div>
+    )
+
+}
+
+export default Store
